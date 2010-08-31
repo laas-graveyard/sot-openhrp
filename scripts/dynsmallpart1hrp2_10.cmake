7d3bf192a77585e8a8bf05d4df59b66876dd2b8b
Francois Bleibel <fbleibel@gmail.com>
2010-08-31 14:56:38 +0900

    Corrected plugin names.

--- scripts/dynsmallpart1hrp2_10.cmake
+++ scripts/dynsmallpart1hrp2_10.cmake
@@ -1,5 +1,5 @@
 run ${CMAKE_INSTALL_PREFIX}/script/base
-loadPlugin sotDynamicHrp2_10${DYN_LIB_EXT}
+loadPlugin dynamic-hrp2_10${DYN_LIB_EXT}
 echo Creating the small dynamic for hrp-2 N10 small
 new DynamicHrp2_10 dyn
 new DynamicHrp2_10 dyn2
--- scripts/dynsmallpart1hrp2_10_old.cmake
+++ scripts/dynsmallpart1hrp2_10_old.cmake
@@ -1,5 +1,5 @@
 run ${CMAKE_INSTALL_PREFIX}/script/base
-loadPlugin sotDynamicHrp2_10_old${DYN_LIB_EXT}
+loadPlugin dynamic-hrp2_10_old${DYN_LIB_EXT}
 echo Creating the small dynamic for hrp-2 N10 small old
 new DynamicHrp2_10_old dyn
 new DynamicHrp2_10_old dyn2
