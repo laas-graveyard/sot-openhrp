7d233672d34d2be84f71cfc52d3bd55a6b5c68e4
Francois Bleibel <fbleibel@gmail.com>
2010-11-08 18:09:32 +0900

    Updated basic documentation.

--- doc/additionalDoc/package.hh
+++ doc/additionalDoc/package.hh
@@ -1,8 +1,52 @@
+/*
+ * Copyright 2010,
+ * François Bleibel,
+ * Olivier Stasse,
+ *
+ * CNRS/AIST
+ *
+ * This file is part of sot-openhrp.
+ * sot-openhrp is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * sot-openhrp is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.  You should
+ * have received a copy of the GNU Lesser General Public License along
+ * with sot-openhrp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 /**
 \mainpage
 
-\section intro_sot-openhrp Introduction
+\section intro Introduction
 
-This package implements ...
+This package builds a plugin for OpenHRP that gives access to the stack of tasks
+framework within. Since the main plugin class is also a dynamicgraph::Entity,
+it is accessible from inside the dynamic-graph framework as any other entity.
+
+\section req Requirements
+This package has the following dependencies:
+\li sot-core
+\li jrl-mal
+\li jrl-walkgen
+\li hrp2Dynamics [optional]
+\li hrp2-10-small [optional]
+
+To download and install these packages, please visit https://github.com/jrl-umi3218.
+
+\section overview API overview
+As most packages based on the dynamic-graph framework (see https://github.com/jrl-umi3218/dynamic-graph),
+the functionality is exposed through entities. Hence .so or .dll (dynamic-link) libraries are
+generated in the dynamic-graph plugins directory.
+
+The following entities are created by this package:\n
+(all entites are placed in the namespace sot::)
+\li sot::StackOfTasks
+
+See each entity's documentation page for more information (when available).
 
 */
